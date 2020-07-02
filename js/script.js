fa96c14905e0638fda64b3c9022598038b3a846e
Marco <Prosperi>
2020-07-02 14:16:44 +0200

    bandiere completate

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ $(document).ready(
         var context = {
           title:singoloFilm.title,
           original_title:singoloFilm.original_title,
-          original_language: singoloFilm.original_language,
+          original_language: stampaBandiera(singoloFilm.original_language),
           vote_average:stelle(singoloFilm.vote_average)
         }
         var html = template(context);
@@ -51,16 +51,33 @@ $(document).ready(
     // funzione che mette in scala 1 a 5 la media voto e assegna
     // una stellina piena per ogni punto, una vuota per i punti mancanti
     function stelle(mediaVoto) {
-    var voto = Math.ceil(mediaVoto / 2);
-    var stelle = '';
-    for (var i = 1; i <= 5; i++) {
+      var voto = Math.ceil(mediaVoto / 2);
+      var stelle = '';
+      for (var i = 1; i <= 5; i++) {
         if (i <= voto) {
           stelle += '<i class="fas fa-star"></i>';
-        }else {
-          stelle += '<i class="far fa-star"></i>';
-        }
+        } else {
+            stelle += '<i class="far fa-star"></i>';
+          }
+      }
+      return stelle;
     }
-    return stelle;
-  }
+
+    function stampaBandiera(bandiera){
+      var lingue = ['ad','ae','af','ag','ai','al','am','ao','aq','ar','as','at','au','aw','ax','az','ba','bb','bd','be','bf','bg',
+      'bh','bi','bj','bl','bm','bn','bo','bq','br','bs','bt','bv','bw','by','bz','ca','cc','cd','cf','cg','ch','ci','ck','cl','cm','cn','co','cr',
+      'cu','cv','cw','cx','cy','cz','de','dj','dk','dm','do','dz','ec','ee','eg','eh','er','es','et','fi','fj','fk','fm','fo','fr','ga','en','gd',
+      'ge','gf','gg','gh','gi','gl','gm','gn','gp','gq','gr','gs','gt','gu','gw','gy','hk','hm','hn','hr','ht','hu','id','ie','il','im','in','io',
+      'iq','ir','is','it','je','jm','jo','jp','ke','kg','kh','ki','km','kn','kp','kr','kw','ky','kz','la','lb','lc','li','lk','lr','ls','lt','lu',
+      'lv','ly','ma','mc','md','me','mf','mg','mh','mk','ml','mm','mn','mo','mp','mq','mr','ms','mt','mu','mv','mw','mx','my','mz','na','nc','ne',
+      'nf','ng','ni','nl','no','np','nr','nu','nz','om','pa','pe','pf','pg','ph','pk','pl','pm','pn','pr','ps','pt','pw','py','qa','re','ro','rs',
+      'ru','rw','sa','sb','sc','sd','se','sg','sh','si','sj','sk','sl','sm','sn','so','sr','ss','st','sv','sx','sy','sz','tc','td','tf','tg','th',
+      'tj','tk','tl','tm','tn','to','tr','tt','tv','tw','tz','ua','ug','um','us','uy','uz','va','vc','ve','vg','vi','vn','vu','wf','ws','ye','yt',
+      'za','zm','zw'];
+      if (lingue.includes(bandiera)) {
+        bandiera = '<img src="img/' + bandiera + '.png" alt="bandiera">';
+      }
+      return bandiera;
+    }
   }
 );
