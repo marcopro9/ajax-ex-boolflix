5ac33210fa72e8e267838ecd7ebd70fd70574e61
Marco <Prosperi>
2020-07-06 11:01:20 +0200

    mile 4 completa

--- js/script.js
+++ js/script.js
@@ -65,7 +65,8 @@ $(document).ready(
           original_name_tv_series:singoloFilm.original_name,
           original_title_film:singoloFilm.original_title,
           original_language:stampaBandiera(singoloFilm.original_language),
-          vote_average:stelle(singoloFilm.vote_average)
+          vote_average:stelle(singoloFilm.vote_average),
+          overview: singoloFilm.overview
         }
         var html = template(context);
         var listaFilm = $('.my_film_list');
@@ -93,8 +94,8 @@ $(document).ready(
       var lingue = ['ad','ae','af','ag','ai','al','am','ao','aq','ar','as','at','au','aw','ax','az','ba','bb','bd','be','bf','bg',
       'bh','bi','bj','bl','bm','bn','bo','bq','br','bs','bt','bv','bw','by','bz','ca','cc','cd','cf','cg','ch','ci','ck','cl','cm','cn','co','cr',
       'cu','cv','cw','cx','cy','cz','de','dj','dk','dm','do','dz','ec','ee','eg','eh','er','es','et','fi','fj','fk','fm','fo','fr','ga','en','gd',
-      'ge','gf','gg','gh','gi','gl','gm','gn','gp','gq','gr','gs','gt','gu','gw','gy','hk','hm','hn','hr','ht','hu','id','ie','il','im','in','io',
-      'iq','ir','is','it','je','jm','jo','jp','ke','kg','kh','ki','km','kn','kp','kr','kw','ky','kz','la','lb','lc','li','lk','lr','ls','lt','lu',
+      'ge','gf','gg','gh','gi','gl','gm','gn','gp','gq','gr','gs','gt','gu','gw','gy','hk','hm','hn','hr','ht','hu','id','ie','il','im','hi','io',
+      'iq','ir','is','it','je','jm','jo','ja','ke','kg','kh','ki','km','kn','kp','kr','kw','ky','kz','la','lb','lc','li','lk','lr','ls','lt','lu',
       'lv','ly','ma','mc','md','me','mf','mg','mh','mk','ml','mm','mn','mo','mp','mq','mr','ms','mt','mu','mv','mw','mx','my','mz','na','nc','ne',
       'nf','ng','ni','nl','no','np','nr','nu','nz','om','pa','pe','pf','pg','ph','pk','pl','pm','pn','pr','ps','pt','pw','py','qa','re','ro','rs',
       'ru','rw','sa','sb','sc','sd','se','sg','sh','si','sj','sk','sl','sm','sn','so','sr','ss','st','sv','sx','sy','sz','tc','td','tf','tg','th',
